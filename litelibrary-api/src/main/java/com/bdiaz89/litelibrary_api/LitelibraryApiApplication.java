f710ec18252d824e5594a2b00aaf95bd36dd6f71
Artu <brayandiaz258f@gmail.com>
2025-11-15 22:44:07 -0500

    Add estructura del proyecto. Cafecito con leche.

---
+++ litelibrary-api/src/main/java/com/bdiaz89/litelibrary_api/LitelibraryApiApplication.java
@@ -0,0 +1,13 @@
+package com.bdiaz89.litelibrary_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LitelibraryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LitelibraryApiApplication.class, args);
+	}
+
+}
